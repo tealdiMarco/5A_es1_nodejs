7e3a1cfc4f78f1d84e44aeb9597a15456f3dd454
TEALDI MARCO <tealdimarco0@gmail.com>
2023-10-13 10:24:51 +0200

    Parse di Url nella barra degli indirizzi

--- server.js
+++ server.js
@@ -2,16 +2,44 @@
 /*
 !   http-> indispensabile se voglio fare client - server
 ?   require-> come import
+!   url-> analizzare e parsificare l'url
+
+
 !   per avviare server uso comando terminale: node server.js
 */
 const http = require("http");
+const url = require("url");
 
 /*
 *   1° parametro-> Richiesta Client
 *   2° parametro-> Risposta Client
 */
 var server = http.createServer(function(request, response){
-    
+/************************* RICHIESTA ********************************/ 
+    let testoRisposta = `
+        url: ${request.url}
+        host: ${request.headers.host}
+        metodo: ${request.method}
+    `;  
+
+    // Estraggo le info dalla stringa completa scritta dal client nella barra delgi indirizzi
+    let indirizzo = request.headers.host + request.url;
+    let infoUrl = url.parse(indirizzo, true);
+
+    testoRisposta += `
+        pathname: ${infoUrl.pathname}
+        parametri: ${infoUrl.search}
+    `;
+
+    // Estraggo i parametri
+    let parametri = infoUrl.query;
+
+    testoRisposta += `
+        action: ${parametri.action}
+        parametro1: ${parametri.p1}
+    `;
+
+/************************ RISPOSTA  *********************************/    
     let header = {"Content-Type":"text/plain"}
     /*
         1° parametro-> codice di risposta (404 | 202)
@@ -20,7 +48,7 @@ var server = http.createServer(function(request, response){
     response.writeHead(200,header);
 
     // Modificare il contenuto del pacchetto (Posso richiamare write anche piu di una volta-> Aggiungo sempre)
-    response.write("Luca frocio per i LEGO");
+    response.write("Luca frocio per i LEGO \n"+ testoRisposta);
 
     // Informo il server che ho terminato di preparare il pacchetto di risposta
     response.end();
@@ -35,3 +63,7 @@ var server = http.createServer(function(request, response){
 server.listen(1337);
 
 console.log("Il server è stato avviato sulla porta 1337");
+/* http://localhost:1337/pippococa.html?action=pluto&p1=paperino */
+
+// Serializzare -> trasformo oggetto in stringa
+// parse -> trasformo stringa in oggetto
